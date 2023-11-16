072c7da6e9dcd785f1d12ce380dde904ba3e97d2
NicolaAdell <81983008+NicolaAdell@users.noreply.github.com>
2023-11-16 11:09:25 -0300

    Delete libs directory

--- libs/inc/init.h
+++
@@ -1,9 +0,0 @@
-#include "pico/stdlib.h"
-#include "hardware/adc.h"
-#include "hardware/pwm.h"
-#include <stdio.h>
-
-void initialize_adc(uint8_t adc_pin, uint8_t adc_input);
-void initialize_pwm(uint8_t pwm_pin);
-int get_position_with_degrees(float degree);
-float get_degrees_with_position(int position);
